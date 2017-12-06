6db3817004262cf529479eb85295228317204557
NinaShao1230 <ss5112@columbia.edu>
2017-12-06 15:59:41 -0500

    Update feature_selection.R

--- lib/feature_selection.R
+++ lib/feature_selection.R
@@ -1,6 +1,6 @@
 ######http://r-statistics.co/Variable-Selection-and-Importance-With-R.html
 
-SpeedDating <- read.csv("~/Desktop/project5/SpeedDating.csv")
+SpeedDating <- read.csv("~/Desktop/project5/SpeedDating_matchrate.csv")
 row.names(SpeedDating)<-SpeedDating[,1]
 SpeedDating<-data.frame(SpeedDating[,-1])
 dim(SpeedDating)
@@ -16,10 +16,10 @@ correlationMatrix <- cor(SpeedDating[,1:29])
 # summarize the correlation matrix
 print(correlationMatrix)
 # find attributes that are highly corrected (ideally >0.75)
-highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.8)
 # print indexes of highly correlated attributes
 print(highlyCorrelated)
-correlation<-as.data.frame(correlationMatrix)
+# correlation<-as.data.frame(correlationMatrix)
 
 
 
@@ -38,12 +38,12 @@ predictors(results)
 plot(results, type=c("g", "o"))
 
 
-###### variable importance based on Generalized cross validation (GCV)------------------------------------------------------------------
-# install.packages("earth")
-library(earth)
-marsModel <- earth( match.prob~ ., data=SpeedDating) # build model
-ev <- evimp (marsModel)
-plot(ev[,1])
+# ###### variable importance based on Generalized cross validation (GCV)------------------------------------------------------------------
+# # install.packages("earth")
+# library(earth)
+# marsModel <- earth( match.prob~ ., data=SpeedDating) # build model
+# ev <- evimp (marsModel)
+# plot(ev[,1])
 
 
 
@@ -56,3 +56,33 @@ boruta_signif <- names(boruta_output$finalDecision[boruta_output$finalDecision %
 print(boruta_signif)  # significant variables
 
 plot(boruta_output, cex.axis=.7, las=2, xlab="", main="Variable Importance")  # plot variable importance
+
+select<-c("age","imprace","income","date","go_out","tvsports","clubbing","reading","concerts","music")
+
+###### ridge regression------------------------------------------------------------------
+install.packages("glmnet")
+library(glmnet)
+library(dplyr)
+# x<-as.matrix(SpeedDating[,-30])
+x<-as.matrix(select(SpeedDating,age,imprace,income,date,go_out,tvsports,clubbing,reading,concerts,music))
+y<-as.vector(SpeedDating[,30])
+train <- sample(1:nrow(x), 425)
+test <- (-train)
+ytest <- y[test]
+lambda <- 10^seq(10, -2, length = 100)
+
+#Ridge Regrerssion 
+ridge.mod <- glmnet(x[train,], y[train], alpha = 0, lambda = lambda)
+#find the best lambda from our list via cross-validation
+cv.out <- cv.glmnet(x[train,], y[train], alpha = 0)
+bestlam <- cv.out$lambda.min
+ridge.pred <- predict(ridge.mod, s = bestlam, newx = x[test,])
+# MSE
+mean((ridge.pred-ytest)^2)
+
+# LASSO 
+lasso.mod <- glmnet(x[train,], y[train], alpha = 1, lambda = lambda)
+lasso.pred <- predict(lasso.mod, s = bestlam, newx = x[test,])
+# MSE 
+mean((lasso.pred-ytest)^2)
+lasso.coef  <- predict(lasso.mod, type = 'coefficients', s = bestlam)[1:6,]
